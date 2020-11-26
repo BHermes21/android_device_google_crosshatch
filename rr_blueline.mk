c92a32776dae4230aeb11da5df612b0551fb7fb5
BHermes21 <BHermes21@sbcglobal.net>
2020-11-26 15:43:16 -0600

    Update rr_blueline.mk

--- rr_blueline.mk
+++ rr_blueline.mk
@@ -1,9 +1,10 @@
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
