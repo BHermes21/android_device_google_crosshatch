3a6355c7c8f06322b876d218a3420ef1683a2ab5
BHermes21 <BHermes21@sbcglobal.net>
2020-11-30 18:22:41 -0600

    Update rr_crosshatch.mk

--- rr_crosshatch.mk
+++ rr_crosshatch.mk
@@ -2,13 +2,19 @@
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common Lineage stuff.
+# Inherit some common rr stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 
--include device/google/crosshatch/crosshatch/device-lineage.mk
+-include device/google/crosshatch/crosshatch/device-rr.mk
+
+# Inherit PixelGApps
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
+# RR Wallpapers
+BUILD_RR_WALLPAPERS := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := rr_crosshatch
@@ -21,7 +27,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crosshatch \
     PRIVATE_BUILD_DESC="crosshatch-user 10 QQ3A.200805.001 6578210 release-keys"
 
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:10/QQ3A.200805.001/6578210:user/release-keys
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
